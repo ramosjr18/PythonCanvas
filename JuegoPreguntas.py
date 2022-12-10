9041ede13feb473bf0acbc6c1afb73ff95c81d30
Daniel Ramos <ramosdarc-18@outlook.com>
2022-12-10 21:56:46 +0100

    espaciado en las preguntas

--- JuegoPreguntas.py
+++ JuegoPreguntas.py
@@ -4,28 +4,28 @@
 score = 0
 
 #preguntas y verificacion de respuestas
-pregunta1 = input("Cual es la capital de Panamá?").lower()
+pregunta1 = input("Cual es la capital de Panamá? ").lower()
 if (pregunta1 == "panama" ):
     print("correcto")
     score +=  1
 else:
     print("Incorrecto")
 
-pregunta2 = input("En que continente queda Costa Rica?").lower()
+pregunta2 = input("En que continente queda Costa Rica? ").lower()
 if (pregunta2 == "america" ):
     print("correcto")
     score +=  1
 else:
     print("Incorrecto")
 
-pregunta3 = input("En que pais esta Barcelona?").lower()
+pregunta3 = input("En que pais esta Barcelona? ").lower()
 if (pregunta3 == "españa" ):
     print("correcto")
     score +=  1
 else:
     print("Incorrecto")
 
-pregunta4 = input("En que continente se encuentra el Everest?").lower()
+pregunta4 = input("En que continente se encuentra el Everest? ").lower()
 if (pregunta4 == "asia" ):
     print("correcto")
     score +=  1
