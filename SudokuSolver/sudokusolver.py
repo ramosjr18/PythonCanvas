bd370520dd433af92d817ea7b8db7b8f0e370258
Daniel Ramos <ramosdarc-18@outlook.com>
2022-12-14 15:07:53 +0100

    Sudoku

---
+++ SudokuSolver/sudokusolver.py
@@ -0,0 +1,21 @@
+#Crear una matriz para el sudoku
+#con este codigo la creo automaticamente, luego solo hago copy paste
+#solver =[]
+#filas = 9
+#columnas = 9
+#for i in range(filas):
+#    solver.append([0]*columnas)
+
+matriz = [[1, 0, 0, 1, 0, 0, 1, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+
+print(matriz)
